befa97e1521b6a6a59e33b6688f1ffcfb10d7526
Troy Dugger <troy@atomicrobot.com>
2022-09-30 16:15:08 -0500

    2 of 5 apis done

--- src/github/github.controller.ts
+++ src/github/github.controller.ts
@@ -19,12 +19,20 @@ type Issue = {
   body: string | null;
 };
 
-const getGithubHeaders = () => {
+const getGithubHeaders = (): AxiosRequestHeaders => {
   const githubHeaders: AxiosRequestHeaders = {
     accept: 'application/vnd.github+json',
     authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
   };
   return githubHeaders;
+};
+
+const stringContainImgTag = (issueBody: string): boolean => {
+  let hasImage = false;
+  if (issueBody?.includes('<img')) {
+    hasImage = true;
+  }
+  return hasImage;
 };
 
 @Controller()
@@ -40,5 +48,19 @@ export class GithubController {
     const response = await axios(options);
     const issue: Issue = response.data as Issue;
     return { id: issue.id, title: issue.title, body: issue.body };
+  }
+
+  @Get('/api/v1/github/:owner/:repo/issue/:issue_number/image')
+  public async doesIssueHaveImage(@Param() params) {
+    const url = `${process.env.GITHUB_URL}/repos/${params.owner}/${params.repo}/issues/${params.issue_number}`;
+    const options: AxiosRequestConfig = {
+      method: 'GET',
+      headers: getGithubHeaders(),
+      url,
+    };
+    const response = await axios(options);
+    const issue: Issue = response.data as Issue;
+    const hasImage = stringContainImgTag(issue.body);
+    return { containsImage: hasImage };
   }
 }
