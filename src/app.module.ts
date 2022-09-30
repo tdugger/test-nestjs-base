32a33723a285c62bfea8a8c11b2258c5cce74718
Troy Dugger <troy@atomicrobot.com>
2022-09-29 22:35:17 -0500

    pull in latest from atomicrobot/nestjs-base commit 0134e10

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { APP_FILTER } from '@nestjs/core';
+import * as OpenApiValidator from 'express-openapi-validator';
+import { OpenApiExceptionFilter } from './filters/openapi-exception.filter';
+import { join } from 'path';
+import { PingController } from './ping/ping.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, PingController],
+  providers: [AppService, { provide: APP_FILTER, useClass: OpenApiExceptionFilter }],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        ...OpenApiValidator.middleware({
+          apiSpec: join(__dirname, './api.yaml'),
+          validateRequests: {
+            allowUnknownQueryParameters: true,
+            coerceTypes: false,
+          },
+          validateResponses: true,
+          validateFormats: 'full',
+        }),
+      )
+      .forRoutes('*');
+  }
+}
